8aa0e33dcb2446923e25d363b8477bf0be8c2246
Andrew <maryshevgit@gmail.com>
2022-05-30 01:11:17 +0300

    Сделал select, в котором можно сортировать по названию и описанию

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import styles from './App.module.scss'
 
 import PostForm from "./components/Posts/PostForm";
 import PostList from "./components/Posts/PostList";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,6 +12,7 @@ function App() {
     {id: 2, title: 'Interesting', body: 'Javascript'},
     {id: 3, title: 'Wonderfull', body: 'React'}
   ])
+  const [selectedSort, setSelectedSort] = useState('')
 
   function removePost(post) {
     setPosts(posts.filter(p => p.id !== post.id))
@@ -20,10 +22,30 @@ function App() {
     setPosts([...posts, newPost])
   }
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))   //мутируем копию массива, т.к. напрямую нельзя мутировать
+  }
+
   return (
     <div className={styles.wrapper}>
       <PostForm create={createPost} />
-      <PostList remove={removePost} posts={posts} title='Список постов'/>
+      <hr style={{margin: '15px 0', width:'100%', backgroundColor:'#4C9EEB', height:'1px'}} />
+      <div className={styles.select}>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue='Сортировка'
+          options={[
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'}
+          ]}
+        />
+      </div>
+      {posts.length !== 0
+        ? <PostList remove={removePost} posts={posts} title='Список постов'/>
+        : <h1>Посты не найдены</h1>
+      }
     </div>
   );
 }
